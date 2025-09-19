2291d2ece7fb405618e86f60cf766c816d5bc3fc
shabad-hora <aashihora20@gmail.com>
2025-09-19 19:34:21 +0100

    Add files via upload

---
+++ typecasting.py
@@ -0,0 +1,12 @@
+a=67
+print("type of a:",type(a))
+b=67.8
+print("type of b:",type(b))
+c="mum"
+print("type of c:",type(c))
+d=False
+print("type of d:",type(d))
+a=float(a)
+print("type of a after typecasting:",type(a))
+b=str(b)
+print("type of b after typecasting:",type(b))
