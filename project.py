ed61192af1a3cb70ab147ca5aa69631d73d63562
shabad-hora <aashihora20@gmail.com>
2025-09-20 18:05:07 +0100

    Add files via upload

---
+++ project.py
@@ -0,0 +1,10 @@
+text=str(input("enter your word"))
+reverse=text[::-1]
+print("the reverse of this word is",reverse)
+text=str(input("enter your word"))
+upper=text[::1]
+print("the uppercase of this word is",text.upper())
+text=str(input("enter your word"))
+lower=text[::1]
+print("the lowercase of this word is",text.lower())
+
